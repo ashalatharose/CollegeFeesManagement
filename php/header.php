53c9e6c516352293c4b590aeb868dad6acb2a4ac
ashalatharose <46776318+ashalatharose@users.noreply.github.com>
2020-01-03 13:38:40 +0530

    Add files via upload

---
+++ php/header.php
@@ -0,0 +1,46 @@
+
+<body>
+<div id="wrapper">
+    <nav class="navbar navbar-default navbar-cls-top " id="hori" role="navigation" style="margin-bottom: 0;text-align: center;color: white;font-family: 'Droid Sans Mono';background:linear-gradient(to right,midnightblue,black)"><h1>College Fees Management System</h1>
+        <div class="navbar-header">
+
+        </div>
+    </nav>
+    <!-- /. NAV TOP  -->
+    <nav class="navbar-default navbar-side" role="navigation" >
+        <div class="sidebar-collapse" >
+            <ul class="nav" id="main-menu" >
+                <li>
+                    <a href="index.php" style="background-color: grey" ><i class="fa fa-dashboard "></i><strong>Dashboard</strong></a>
+                </li>
+
+                <li>
+                    <a href="branch.php" ><i class="fa fa-university "></i><strong>Branch</strong></a>
+                </li>
+
+
+                <li>
+                    <a href="student.php"><i class="fa fa-users "></i><strong>Student</strong></a>
+                </li>
+                <li>
+                    <a href="fees.php" ><i class="fa fa-dollar "></i> <strong>Fees</strong></a>
+                </li>
+                <li>
+                    <a href="notes.php" ><i class="fa fa-book "></i> <strong>Notes</strong></a>
+                </li>
+                <li>
+                    <a href="report.php"><i class="fa fa-file-text "></i><strong>Report</strong> </a>
+                </li>
+                <li>
+                    <a href="setting.php"><i class="fa fa-cogs "></i><strong>Setting</strong></a>
+                </li>
+                <li>
+                    <a href="user.php" ><i class="fa fa-question-circle "></i><strong>Contact Queries</strong></a>
+                </li>
+                <li>
+                    <a href="logout.php"><i class="fa fa-power-off "></i><strong>Logout</strong></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- /. NAV SIDE  -->
---
+++ php/userheadapp.php
@@ -0,0 +1,39 @@
+
+<body>
+<div id="wrapper">
+    <nav class="navbar navbar-default navbar-cls-top " id="hori" role="navigation" style="margin-bottom: 0;text-align: center;color: white;font-family: 'Droid Sans Mono';background:linear-gradient(to right,midnightblue,black)"><h1>College Fees Management System</h1>
+        <div class="navbar-header">
+        </div>
+    </nav>
+    <!-- /. NAV TOP  -->
+
+
+    <nav class="navbar-default navbar-side" role="navigation" >
+        <div class="sidebar-collapse" >
+            <ul class="nav" id="main-menu" >
+                <li>
+                    <a href="../userindex.php" style="background-color: grey" ><i class="fa fa-dashboard "></i><strong>Dashboard</strong></a>
+                </li>
+
+                <li >
+                    <a href="applicationform.php" ><i class="fa fa-file-text "></i><strong>Application Form</strong></a>
+                </li>
+
+                <li>
+                    <a href="../usernotes.php"><i class="fa fa-book "></i><strong>Notes</strong></a>
+                </li>
+
+                <li>
+                    <a href="../userreport.php"><i class="fa fa-user "></i><strong>Report</strong></a>
+                </li>
+
+                <li>
+                    <a href="../usersetting.php"><i class="fa fa-cogs "></i><strong>Password Setting</strong></a>
+                </li>
+                <li>
+                    <a href="../logout.php"><i class="fa fa-power-off "></i><strong>Logout</strong></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- /. NAV SIDE  -->
